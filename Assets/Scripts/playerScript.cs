f02fc9d539d56031f2459e480de13c73db47a172
Leo <leofeitosa@live.com>
2019-05-31 21:22:33 -0300

    ajuste das mecanicas

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -6,11 +6,13 @@ public class playerScript : MonoBehaviour
 {
     private     Animator    playerAnimator;
     private     Rigidbody2D playerRb;
+    private     float       h, v;
 
     public      Transform   groundCheck; //objeto responsavel em deectar se o prsonagem esta sobre uma superficie
     public      float       speed; //velocidade de movimentacao do personagem
     public      float       jumpForce; //forca aplicada para gerar o pulo do personagem
     public      bool        Grounded; //indica se o personagem esta pisando em alguma superficie
+    public      bool        attacking; //indica de o personagem esta atacando
     public      bool        loockLeft; //indica se o personagem esta virado para a esquerda
     public      int         idAnimation; //indica o id da animacao
 
@@ -24,26 +26,31 @@ public class playerScript : MonoBehaviour
     private void FixedUpdate()
     {
         Grounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
+        playerRb.velocity = new Vector2(h * speed, playerRb.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
 
-        if(h > 0 && loockLeft)
+        if(h > 0 && loockLeft && !attacking)
         {
             flip();
         }
-        else if(h< 0 && !loockLeft)
+        else if(h< 0 && !loockLeft && !attacking)
         {
             flip();
         }
 
-        if(v != 0)
+        if(v < 0)
         {
             idAnimation = 2;
+            if (Grounded)
+            {
+                h = 0;
+            }
         }
         else if (h != 0)
         {
@@ -53,7 +60,7 @@ public class playerScript : MonoBehaviour
             idAnimation = 0;
         }
 
-        if (Input.GetButtonDown("Fire1") && v >= 0) {
+        if (Input.GetButtonDown("Fire1") && v >= 0 && !attacking) {
             playerAnimator.SetTrigger("atack");
         }
 
@@ -61,8 +68,11 @@ public class playerScript : MonoBehaviour
             playerRb.AddForce(new Vector2(0, jumpForce));
         }
 
-        playerRb.velocity = new Vector2(h * speed, playerRb.velocity.y);
-
+        if (attacking && Grounded)
+        {
+            h = 0;
+        }
+        
         playerAnimator.SetBool("grounded", Grounded);
         playerAnimator.SetInteger("idAnimation", idAnimation);
         playerAnimator.SetFloat("speedY", playerRb.velocity.y);
@@ -74,5 +84,19 @@ public class playerScript : MonoBehaviour
         float x = transform.localScale.x;
         x *= -1;
         transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void attack(int atk)
+    {
+        switch (atk)
+        {
+            case 0:
+                attacking = false;
+                break;
+
+            case 1:
+                attacking = true;
+                break;
+        }
     }
 }
