9f36da9a3f59dd8bf5e2f3b3c8577c282c9ef7ef
LeoFeitosa <you@example.com>
2020-01-01 19:02:23 -0300

    parallax

---
+++ Assets/Scripts/paralax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paralax : MonoBehaviour
+{
+    public Transform background;
+    public float paralaxScale;
+    public float velocidade;
+
+    public Transform cam;
+    public Vector3 previewCamPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+        previewCamPosition = cam.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float paralaxX = (previewCamPosition.x - cam.position.x) * paralaxScale;
+        float bgTargetX = background.position.x - paralaxX;
+
+        Vector3 bgPos = new Vector3(bgTargetX, background.position.y, background.position.z);
+        background.position = Vector3.Lerp(background.position, bgPos, velocidade * Time.deltaTime);
+
+        previewCamPosition = cam.position;
+    }
+}
